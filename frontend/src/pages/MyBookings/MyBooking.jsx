a9dfbd7b44ef9da11ff7ebfc7dd73e41303c03b2
ashusatp <92241390+ashusatp@users.noreply.github.com>
2023-12-29 22:31:34 +0530

    booking page updated

--- frontend/src/pages/MyBookings/MyBooking.jsx
+++ frontend/src/pages/MyBookings/MyBooking.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import styles from "./MyBooking.module.css";
 import BookingCard from "../../components/BookingCard/BookingCard";
 import Loader from "../Loader/Loader";
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { setAllBookings } from "../../stores/allBookingsSlice";
+// import { setAllBookings } from "../../stores/allBookingsSlice";
 const MyBooking = () => {
   const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+  const [bookings, setBookings] = useState([]);
+  const [status, setStatus] = useState("pending");
+
   useEffect(() => {
     (async () => {
       try {
@@ -16,7 +18,9 @@ const MyBooking = () => {
           `${process.env.REACT_APP_API_URL}/api/user/my-bookings`,
           { withCredentials: true }
         );
-        dispatch(setAllBookings(data.data));
+        // dispatch(setAllBookings(data.data));
+        setBookings(data.data);
+        console.log(data.data);
         setLoading(false);
       } catch (error) {
         console.log(error);
@@ -25,22 +29,26 @@ const MyBooking = () => {
     })();
   }, []);
 
-  const bookings = useSelector((state) => state.allBookings.bookings);
-
   return (
     <div className={styles.wrapper}>
       {loading ? (
         <Loader />
       ) : (
         <div className={styles.innerWrapper}>
-          {bookings &&
-            bookings.map((booking, index) => {
+          {/* drop down */}
+          {bookings.map((booking, index) => {
+            if (booking.status === status) {
               return (
-                <Link style={{textDecoration: 'none', color: "#000"}} key={index} to={`/my-booking/${booking._id}`}>
-                  <BookingCard booking={booking}/>
+                <Link
+                  style={{ textDecoration: "none", color: "#000" }}
+                  key={index}
+                  to={`/my-booking/${booking._id}`}
+                >
+                  <BookingCard booking={booking} />
                 </Link>
               );
-            })}
+            }
+          })}
         </div>
       )}
     </div>
